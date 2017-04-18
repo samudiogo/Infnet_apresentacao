809342f80d9d2e3c2df401f3423e334e33d50b3b
Supremo Samuel Diogo <samudiogo@gmail.com>
2017-04-18 19:04:02 -0300

    implementação da classe aluno

---
+++ src/negocio/Aluno.java
@@ -0,0 +1,49 @@
+package negocio;
+
+public class Aluno extends UsuarioBase {
+	private String matricula;
+	private int cr;
+
+	public Aluno() {
+	}
+
+	public Aluno(String mat) {
+		setMatricula(mat);
+	}
+
+	public Aluno(String mat, int cr) {
+		setMatricula(mat);
+		setCr(cr);
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public int getCr() {
+		return cr;
+	}
+
+	public void setCr(int cr) {
+		this.cr = cr;
+	}
+
+	@Override
+	public void show() {
+		System.out.println(this.toString());
+
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Aluno [matricula=").append(matricula).append(",").append(super.toString()).append(", cr=")
+				.append(cr).append("]");
+		return builder.toString();
+	}
+
+}
